70945e41a2003a06058e1eb4fa298fdf4c9f20dc
Vincent_tt <evgeniy161102@gmail.com>
2025-02-10 21:10:41 +0300

    DZ lesson-1

---
+++ lesson-1/dz/dz.js
@@ -0,0 +1,51 @@
+// const user = {
+//   name: "John",
+//   age: 30,
+//   role: "Administrator",
+// };
+
+// const objEntries = Object.entries(user);
+// console.log(objEntries);
+
+// const objKeys = Object.keys(user);
+// console.log(objKeys);
+
+// const objValues = Object.values(user);
+// console.log(objValues);
+
+// const user = {
+//   name: "John",
+//   age: 30,
+// };
+
+// const user1 = {
+//   lastName: "Doe",
+//   role: "admin",
+// };
+
+// user1.__proto__ = user;
+
+// for (let key in user1) {
+//   if (user1.hasOwnProperty(key)) {
+//     console.log(key);
+//   }
+// }
+
+// const posts = {
+//     2: "Пост 2",
+//     3: "Пост 3",
+//     1: "Пост 1",
+// }
+
+// console.log(posts);
+
+// const user = {
+//   name: "John",
+//   age: 30,
+//   position: "manager",
+//   salary: 5000,
+// };
+
+// for (let key in user) {
+//   console.log(`${key}: ${user[key]}`);
+// }
